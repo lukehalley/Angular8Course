ffca1652c6452065f28551c90e90b2986c425515
lukehalley <luke123halley@gmail.com>
2019-09-08 14:49:32 +0100

    done assignment

--- angular-eight/src/app/app.module.ts
+++ angular-eight/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { AlertsComponent } from './alerts/alerts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
+    AlertsComponent
   ],
   imports: [
     BrowserModule
