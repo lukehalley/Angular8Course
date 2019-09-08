4571d087f2492cde5397caef898a5c5ffe5c3f70
lukehalley <luke123halley@gmail.com>
2019-09-08 17:45:09 +0100

    section 2 finished

--- angular-eight/src/app/server/server.component.ts
+++ angular-eight/src/app/server/server.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
 })
 
 export class ServerComponent {
+  serverId = 10;
+  serverStatus = 'offline';
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  getServerId() {
+    if (this.serverId <= 10) {
+      return this.serverId;
+    } else {
+      return 11;
+    }
+
+  }
+
+  getColor() {
+    if (this.serverStatus === 'online') {
+      return 'green';
+    } else {
+      return 'red';
+    }
+  }
+
+  getClass() {
+    if (this.serverStatus === 'online') {
+      return 'online';
+    } else {
+      return 'offline';
+    }
+  }
 
 }
